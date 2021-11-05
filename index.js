43be0fff93bf9808e3afe4882e1846ce36f7b745
Jhaycee <43661903+johncarlosllaneta@users.noreply.github.com>
2021-11-05 14:41:24 +0800

    make it better

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const multer = require("multer");
 const path = require("path");
+const config = require('./config');
+const pino = require('express-pino-logger')();
+const { videoToken } = require('./tokens');
 app.use(express.json());
 app.use(cors());
 
@@ -17,6 +20,36 @@ app.use(
     extended: true,
   })
 );
+app.use(pino);
+
+const sendTokenResponse = (token, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(
+    JSON.stringify({
+      token: token.toJwt()
+    })
+  );
+};
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.get('/video/token', (req, res) => {
+  const identity = req.query.identity;
+  const room = req.query.room;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+
+});
+app.post('/video/token', (req, res) => {
+  const identity = req.body.identity;
+  const room = req.body.room;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
 
 //Table of Contents
 //CreateConnection
