39eefc9e1d6cce1f5a25adc7366d4914e2902ef1
Jhaycee <johnllaneta05@gmail.com>
2022-04-15 00:15:15 +0800

    update index.js

--- index.js
+++ index.js
@@ -73,23 +73,23 @@ app.post("/video/token", (req, res) => {
 // Email Verification
 
 //--------------------------------------------------------------------------//
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "terravet",
-});
-
-// const db = mysql.createPool({
-//   connectionLimit: 1000,
-//   connectTimeout: 60 * 60 * 1000,
-//   acquireTimeout: 60 * 60 * 1000,
-//   timeout: 60 * 60 * 1000,
-//   host: "us-cdbr-east-05.cleardb.net",
-//   user: "bdb2dd6ba41ba9",
-//   password: "9542972d",
-//   database: "heroku_9d423aff4dc7247",
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "terravet",
 // });
+
+const db = mysql.createPool({
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "bdb2dd6ba41ba9",
+  password: "9542972d",
+  database: "heroku_9d423aff4dc7247",
+});
 
 // console.log(db);
 
@@ -841,9 +841,9 @@ function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
   var a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c; // Distance in km
   return Math.round(d * 10) / 10;
@@ -936,14 +936,16 @@ app.post("/vetclinic/insert", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
+  const lat = req.body.latitude;
+  const lon = req.body.longitude;
 
   bcrypt.hash(password, saltRounds, (err, hash) => {
     bcrypt.hash(name, saltRounds, (err, hashId) => {
       const sqlQuery =
-        "INSERT INTO vet_clinic (email,password,vet_name,vet_status,vetid,isOnline) VALUES (?,?,?,?,?,?)";
+        "INSERT INTO vet_clinic (email,password,vet_name,vet_status,vetid,isOnline,longitude,Latitude) VALUES (?,?,?,?,?,?,?,?)";
       db.query(
         sqlQuery,
-        [email, hash, name, "Unverified", hashId.replace("/", "5"), 0],
+        [email, hash, name, "Unverified", hashId.replace("/", "5"), 0, lon, lat],
         (err, result) => {
           if (err !== null) {
             console.log(err);
@@ -1960,7 +1962,7 @@ app.put("/reservation/cancel", (req, res) => {
 
       const updateProduct =
         "UPDATE products SET quantity = ? WHERE product_id = ?";
-      db.query(updateProduct, [deduced, product_id], (err, result) => {});
+      db.query(updateProduct, [deduced, product_id], (err, result) => { });
     });
 
     console.log(err);
@@ -3146,7 +3148,7 @@ app.post("/sendSMS/:phoneNumber", (req, res) => {
         db.query(
           sqlQueryInsert,
           [phoneNumber, verificationCode],
-          (err, result) => {}
+          (err, result) => { }
         );
       } else {
         console.log("invalid number");
@@ -4972,7 +4974,7 @@ app.get("/vetadmin/notification/length/:vetid", (req, res) => {
 });
 
 //notif details
-pp.get("/vetadmin/notification/:vetid", (req, res) => {
+app.get("/vetadmin/notification/:vetid", (req, res) => {
   const vetid = req.params.vetid;
   // console.log(pet_owner_id);
   const sqlQuery =
