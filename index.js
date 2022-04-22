4f99a7491d76d8e253f21435a747139865cab287
MercadoJimuel <43661718+MRYOSO78@users.noreply.github.com>
2022-04-23 00:34:20 +0800

    updateds

--- index.js
+++ index.js
@@ -2475,10 +2475,11 @@ app.get("/petowner/vaccination/:pet_id", (req, res) => {
 //for pet consultation record
 app.get("/petowner/consultation/:pet_id", (req, res) => {
   const pet_id = req.params.pet_id;
+    console.log("pet id"+pet_id);
   const sqlQuery =
-    "SELECT * FROM vet_clinic JOIN vet_doctors ON vet_clinic.vet_admin_id = vet_doctors.vet_admin_id JOIN consultation ON consultation.vet_admin_id = vet_clinic.vet_admin_id JOIN pets ON pets.pet_id = consultation.pet_id JOIN pet_owners ON pet_owners.pet_owner_id = pets.pet_owner_id WHERE pets.pet_id =?";
+    "SELECT * FROM vet_clinic JOIN consultation ON vet_clinic.vetid = consultation.vetid JOIN pets ON pets.pet_id = consultation.pet_id JOIN pet_owners ON pet_owners.pet_owner_id = pets.pet_owner_id WHERE pets.pet_id =?";
   db.query(sqlQuery, pet_id, (err, result) => {
-    // console.log(result);
+    console.log(result);
     res.send(result);
   });
 });
